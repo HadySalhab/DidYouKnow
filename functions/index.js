2379c03fa9013bf8b041f4dae67d7f63668e9829
Hady Salhab <hadysalhab1@gmail.com>
2020-09-30 20:51:20 +1000

    Create getFacts handler

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const constants = require("./constants");
+
+// default app is set in .firebaserc file
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,4 +12,23 @@ const functions = require("firebase-functions");
 exports.helloWorld = functions.https.onRequest((request, response) => {
 	functions.logger.info("Hello logs!", { structuredData: true });
 	response.send("Hello from Firebase!");
+});
+
+exports.getFacts = functions.https.onRequest(async (request, response) => {
+	const facts = [];
+	try {
+		const factsRef = admin
+			.firestore()
+			.collection(constants.factsCollectionName);
+		const snapshot = await factsRef.get();
+		snapshot.forEach((doc) => {
+			facts.push(doc.data());
+		});
+		return response.json({
+			success: true,
+			data: facts,
+		});
+	} catch (error) {
+		console.log(error);
+	}
 });
