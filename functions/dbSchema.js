e066e68349d5371e16a052f44036223aa2ba030a
Hady Salhab <hadysalhab1@gmail.com>
2020-09-30 23:18:16 +1000

    Add dbSchema, define facts schema

---
+++ functions/dbSchema.js
@@ -0,0 +1,12 @@
+let db = {
+	facts: [
+		{
+			owner: "owner",
+			question: "Fact Question",
+			answer: "Fact Answer",
+			createdAt: "2020-09-30T13:12:22.470Z",
+			totalLikes: 75,
+			totalComments: 10,
+		},
+	],
+};
