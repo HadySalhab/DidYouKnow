3865dd43a4c09547e2ef22ccb8771fd4cf28d7cf
Hady Salhab <hadysalhab1@gmail.com>
2020-10-01 17:45:55 +1000

    define user schema

--- functions/dbSchema.js
+++ functions/dbSchema.js
@@ -1,12 +1,25 @@
 let db = {
 	facts: [
 		{
-			owner: "owner",
+			username: "username",
 			question: "Fact Question",
 			answer: "Fact Answer",
 			createdAt: "2020-09-30T13:12:22.470Z",
 			totalLikes: 75,
 			totalComments: 10,
+		},
+	],
+	users: [
+		{
+			authID: "BPwFympP2CUPvUR0tJcjG6gfplq2",
+			email: "user@gmail.com",
+			username: "user123",
+			createdAt: "2020-09-30T13:12:22.470Z",
+			imageUrl:
+				"https://firebasestorage.googleapis.com/v0/b/did-you-know-e5c9c.appspot.com/o/hjsalhab.png?alt=media",
+			bio: "Hello my name is x and i'm a software developer",
+			location: "Sydney, AU",
+			website: "www.user.com",
 		},
 	],
 };
