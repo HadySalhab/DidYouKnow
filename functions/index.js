9d50f2710bba7a63d0ed5e4cc983c6c7e93209b4
Hady Salhab <hadysalhab1@gmail.com>
2020-10-01 23:07:48 +1000

    create event listeners for notification

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,7 @@ const express = require("express");
 const factsRoute = require("./routes/facts");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
-
+const db = require("./config/db");
 const app = express();
 
 // Body Parser
@@ -26,3 +26,56 @@ app.use("/users", usersRoute);
 app.use(errorHandler);
 
 exports.api = functions.region("australia-southeast1").https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+	.region("australia-southeast1")
+	.firestore.document("likes/{id}")
+	.onCreate(async (likeDocSnapshot) => {
+		const factDocSnapshot = await db
+			.doc(`/facts/${likeDocSnapshot.data().fact}`)
+			.get();
+		try {
+			await db.doc(`/notifications/${likeDocSnapshot.id}`).set({
+				createdAt: new Date().toISOString(),
+				receiver: factDocSnapshot.data().username,
+				sender: likeDocSnapshot.data().username,
+				type: "like",
+				read: false,
+				fact: factDocSnapshot.id,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+exports.deleteNotificationOnUnlike = functions
+	.region("australia-southeast1")
+	.firestore.document("likes/{id}")
+	.onDelete(async (likeDocSnapshot) => {
+		try {
+			await db.doc(`/notifications/${likeDocSnapshot.id}`).delete();
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+exports.createNotificationOnComment = functions
+	.region("australia-southeast1")
+	.firestore.document("comments/{id}")
+	.onCreate(async (commentDocSnapshot) => {
+		const factDocSnapshot = await db
+			.doc(`/facts/${commentDocSnapshot.data().fact}`)
+			.get();
+		try {
+			await db.doc(`/notifications/${commentDocSnapshot.id}`).set({
+				createdAt: new Date().toISOString(),
+				receiver: factDocSnapshot.data().username,
+				sender: commentDocSnapshot.data().username,
+				type: "comment",
+				read: false,
+				fact: factDocSnapshot.id,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	});
