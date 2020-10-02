c7f3062007e17a45930d4f6a1dcd067f84d5a037
Hady Salhab <hadysalhab1@gmail.com>
2020-10-02 12:01:23 +1000

    update dbSchema

--- functions/dbSchema.js
+++ functions/dbSchema.js
@@ -1,7 +1,8 @@
 let db = {
 	facts: [
 		{
-			username: "username",
+			id: "bFlsaSzSrFOdfFisda", //Primary-key
+			username: "username", //Reference to user doc
 			question: "Fact Question",
 			answer: "Fact Answer",
 			createdAt: "2020-09-30T13:12:22.470Z",
@@ -11,15 +12,41 @@ let db = {
 	],
 	users: [
 		{
-			authId: "BPwFympP2CUPvUR0tJcjG6gfplq2",
+			authId: "BPwFympP2CUPvUR0tJcjG6gfplq2", //Reference to firebase auth collection
 			email: "user@gmail.com",
-			username: "user123",
+			username: "user123", //Primary-key
 			createdAt: "2020-09-30T13:12:22.470Z",
 			imageUrl:
 				"https://firebasestorage.googleapis.com/v0/b/did-you-know-e5c9c.appspot.com/o/hjsalhab.png?alt=media",
 			bio: "Hello my name is x and i'm a software developer",
 			location: "Sydney, AU",
 			website: "www.user.com",
+		},
+	],
+	comments: [
+		{
+			username: "user123", //Reference to user doc,
+			fact: "bFlsaSzSrFOdfFisda", //Reference to fact doc
+			body: "Nice one..",
+			createdAt: "2020-09-30T13:12:22.470Z",
+			id: "BFwFxxmpD2CUPvUYYYfplq2", //Primary key
+		},
+	],
+	likes: [
+		{
+			username: "user123", //Reference to user doc,
+			fact: "bFlsaSzSrFOdfFisda", //Reference to fact doc
+			id: "BFwFxxmpD2CUPvUYYYfplq2", //Primary key
+		},
+	],
+	notifications: [
+		{
+			createdAt: "2020-09-30T13:12:22.470Z",
+			receiver: "user123", //Reference to user doc,
+			sender: "user321", //Reference to user doc,
+			type: "like | comment",
+			read: false,
+			fact: "bFlsaSzSrFOdfFisda", //Reference to fact doc
 		},
 	],
 };
