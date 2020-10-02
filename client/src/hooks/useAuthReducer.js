54d9cf450c2eb6bf2675e44fe56660c0c702c7a1
Hady Salhab <hadysalhab1@gmail.com>
2020-10-02 16:41:04 +1000

    create useAuthReducer hook

---
+++ client/src/hooks/useAuthReducer.js
@@ -0,0 +1,76 @@
+import { useReducer } from "react";
+
+import { useInputSetter } from "../utils/functions";
+
+const initialState = {
+	username: "",
+	email: "",
+	password: "",
+	confirmPassword: "",
+	loading: false,
+	error: "",
+};
+
+function authReducer(state, action) {
+	switch (action.type) {
+		case "SET_USERNAME":
+			return { ...state, username: action.payload };
+		case "SET_EMAIL":
+			return { ...state, email: action.payload };
+		case "SET_PASSWORD":
+			return { ...state, password: action.payload };
+		case "SET_CONFIRM_PASSWORD":
+			return { ...state, passwordConfirm: action.payload };
+		case "LOADING":
+			return { ...state, loading: true, error: "" };
+		case "ERROR":
+			return { ...state, loading: false, error: action.payload };
+		case "RESET":
+			return initialState;
+		default:
+			return state;
+	}
+}
+
+const useAuthReducer = () => {
+	const [state, dispatch] = useReducer(authReducer, initialState);
+	const setUsername = useInputSetter((username) => {
+		dispatch({
+			type: "SET_USERNAME",
+			payload: username,
+		});
+	});
+
+	const setEmail = useInputSetter((email) => {
+		dispatch({
+			type: "SET_EMAIL",
+			payload: email,
+		});
+	});
+	const setPassword = useInputSetter((password) => {
+		dispatch({
+			type: "SET_PASSWORD",
+			payload: password,
+		});
+	});
+	const setPasswordConfirm = useInputSetter((passwordConfirm) => {
+		dispatch({
+			type: "SET_PASSWORD_CONFIRM",
+			payload: passwordConfirm,
+		});
+	});
+	const setLoading = () => dispatch({ type: "LOADING" });
+	const setError = (error) => dispatch({ type: "ERROR", payload: error });
+	const setReset = () => dispatch({ type: "RESET" });
+
+	return {
+		...state,
+		setUsername,
+		setEmail,
+		setPassword,
+		setPasswordConfirm,
+		setLoading,
+		setError,
+		setReset,
+	};
+};
