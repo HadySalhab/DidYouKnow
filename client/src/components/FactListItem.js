6e0dcdf82909deab20f2eb0a52ccd6ba31e189ce
Hady Salhab <hadysalhab1@gmail.com>
2020-10-04 00:41:52 +1000

    FactListItem Preliminary Design

---
+++ client/src/components/FactListItem.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+// MUI
+import Box from "@material-ui/core/Box";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import ChatIcon from "@material-ui/icons/Chat";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
+import Card from "@material-ui/core/Card";
+
+const useStyles = makeStyles((theme) => ({
+	...theme.spreadThis,
+	fact: {
+		maxWidth: "600px",
+		margin: "0 auto",
+		"&:hover": {
+			cursor: "pointer",
+		},
+	},
+	date: {
+		fontStyle: "italic",
+		marginLeft: "auto",
+	},
+	iconButton: {
+		padding: 0,
+		marginLeft: "5px",
+	},
+}));
+
+const FactListItem = () => {
+	const classes = useStyles();
+	return (
+		<Card className={classes.fact} variant="outlined" square="true">
+			<Box p={2}>
+				<Box display="flex">
+					<Avatar>R</Avatar>
+					<Box flexDirection="column" ml={2} flexGrow="1">
+						<Box mb={1}>
+							<Box display="flex">
+								<Typography variant="subtitle2" component="p">
+									What is the longest recorded flight of a chicken ?
+								</Typography>
+								<Typography
+									variant="outline"
+									component="p"
+									className={classes.date}
+								>
+									5 days ago.
+								</Typography>
+							</Box>
+							<Typography variant="body2" component="p">
+								13 seconds
+							</Typography>
+						</Box>
+						{/*  */}
+						<Box display="flex">
+							<Box>
+								<Typography variant="body2" component="span">
+									10
+								</Typography>
+								<IconButton className={classes.iconButton}>
+									<FavoriteIcon />
+								</IconButton>
+							</Box>
+
+							<Box ml={1}>
+								<Typography variant="body2" component="span">
+									10
+								</Typography>
+								<IconButton className={classes.iconButton}>
+									<ChatIcon />
+								</IconButton>
+							</Box>
+						</Box>
+					</Box>
+				</Box>
+			</Box>
+		</Card>
+	);
+};
+
+export default FactListItem;
