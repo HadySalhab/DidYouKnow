2c202830c5736d9026cfd942a0bdd5f4b78e8182
Hady Salhab <hadysalhab1@gmail.com>
2020-10-04 23:01:26 +1100

    display loading in factlist

--- client/src/components/FactList.js
+++ client/src/components/FactList.js
@@ -2,18 +2,34 @@ import React from "react";
 
 // MUI
 import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 // Components
 import FactListItem from "./FactListItem";
 
-const FactList = ({ facts }) => {
+const useStyles = makeStyles((theme) => ({}));
+
+const FactList = ({ facts, loading, error }) => {
+	const classes = useStyles();
 	return (
 		<div>
-			{facts.map((fact) => (
-				<Box mb={1}>
-					<FactListItem />
+			{loading && (
+				<Box display="flex" justifyContent="center" alignItems="center">
+					<CircularProgress
+						color="secondary"
+						size={30}
+						className={classes.progress}
+					/>
 				</Box>
-			))}
+			)}
+			{error && <p>error</p>}
+			{facts &&
+				facts.map((fact) => (
+					<Box mb={1}>
+						<FactListItem />
+					</Box>
+				))}
 		</div>
 	);
 };
