ce90affe5b89572e8cbcadd3a3fa804213e86bce
Hady Salhab <hadysalhab1@gmail.com>
2020-10-06 10:37:42 +1100

    create getFact action

--- client/src/redux/actions/factsActions.js
+++ client/src/redux/actions/factsActions.js
@@ -1,10 +1,24 @@
-import { GET_ALL_FACTS } from "../types";
+import { GET_ALL_FACTS, GET_PROFILE } from "../types";
 import axios from "axios";
-// Get all facts
+import _ from "lodash";
+// @desc      Get all facts
+// @route     GET /facts
 export const getAllFacts = () => async (dispatch) => {
 	const response = await axios.get("/facts");
 	dispatch({
 		type: GET_ALL_FACTS,
 		payload: response.data.data,
+	});
+};
+
+// @desc      Get a single fact
+// @route     GET /facts/:factId
+export const getFact = (factId) => async (dispatch) => {
+	const response = await axios.get(`/facts/${factId}`);
+	const factDetails = response.data.data;
+	const { username } = _.pick(factDetails, ["username"]);
+	dispatch({
+		type: GET_PROFILE,
+		payload: username,
 	});
 };
